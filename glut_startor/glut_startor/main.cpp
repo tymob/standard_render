c85efe7c344fce777af5a85d56f7c8b8229e441d
Administrator <admin@example.com>
2016-12-01 11:41:21 +0900

    test

---
+++ glut_startor/glut_startor/main.cpp
@@ -0,0 +1,16 @@
+/*C5-1.c*/
+
+/*環境変数とかを考えずにプロジェクト単位で動く。
+OPENGL特有のライブラリで、リンクエラーがあるものは消しても動くはず。
+なので、コピーするのは、includeの文章以外いれれば大丈夫。defineはもちろんいれる。*/
+#include <stdlib.h>
+#include <glut.h>
+#include <math.h>
+#include <iostream>
+
+int main()
+{
+	printf("Test\n");
+	return 0;
+}
+/*作成->g1232219@tcu.ac.jp*/
