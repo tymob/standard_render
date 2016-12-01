5eb2dbd2fdcac4a3c434ae7ee9481de2f3104c8a
HideoMiyachi <miyachi@tcu.ac.jp>
2016-12-01 11:44:25 +0900

    Add files via upload

---
+++ glut_startor/glut_startor/a1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct prPoints {
+	int num;
+	char *name;
+	float *xyz;
+	float *color;
+	float *normal;
+	float *val;
+} prPoints;
+
+prPoints *prCreatePtObj( char *, int);
+
+main(){
+
+	prPoints *b;
+	prPoints a;
+
+	a.num =5;
+	printf(" a.num = %d\n", a.num);
+	b = prCreatePtObj("myObj",5);
+
+}
